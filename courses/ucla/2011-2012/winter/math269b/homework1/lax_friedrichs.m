b7d1b9acb960386d2cb60c8f1ce480df083aa37a
Jeffrey Hellrung <jeffrey.hellrung@gmail.com>
2012-03-10 22:38:56 -0800

    updating solutions1 and code

--- courses/ucla/2011-2012/winter/math269b/homework1/lax_friedrichs.m
+++ courses/ucla/2011-2012/winter/math269b/homework1/lax_friedrichs.m
@@ -1,6 +1,6 @@
-function u = lax_friedrichs(u, a, k, h)
+function v = lax_friedrichs(v, a, k, h)
     % (1/k)*(v^{n+1}_m - (1/2)*(v^n_{m+1} + v^n_{m-1})) ...
     %     + (a/(2*h))*(v^n_{m+1} - v^n_{m-1}) = 0
-    u = ((1 - a*k/h)/2)*[u(2:end) u(2)] ...
-      + ((1 + a*k/h)/2)*[u(end-1) u(1:end-1)];
+    v = ((1 - a*k/h)/2)*[v(2:end) v(2)] ...
+      + ((1 + a*k/h)/2)*[v(end-1) v(1:end-1)];
 endfunction
--- courses/ucla/2011-2012/winter/math269b/homework1/test_convergence.m
+++ courses/ucla/2011-2012/winter/math269b/homework1/test_convergence.m
@@ -4,7 +4,7 @@ function test_convergence(a, T, u0, scheme, scheme_name, hs, k_of_h, norm_p)
         norm_p = 2;
     endif
 
-    e = zeros([1 length(hs)]);
+    e = zeros(size(hs));
 
     for i = 1:length(hs)
         h = hs(i);
@@ -14,15 +14,15 @@ function test_convergence(a, T, u0, scheme, scheme_name, hs, k_of_h, norm_p)
         N = ceil(T/k);
         k = T/N;
         x = (0:M)/M;
-        u = u0(x);
+        v = u0(x);
         for n = 1:N
-            u = scheme(u, a, k, h);
+            v = scheme(v,a,k,h);
         endfor
-        e(i) = sqrt(h) * norm((u0(mod(x - a*T, 1)) - u)(1:end-1), norm_p);
+        e(i) = sqrt(h) * norm((u0(mod(x-a*T,1)) - v)(1:end-1), norm_p);
     endfor
 
     p = polyfit(log(hs), log(e), 1);
-    plot(log(hs), log(e), "o", log(hs), p(1)*log(hs) + p(2));
+    plot(log(hs), log(e), "o", log(hs), polyval(p,log(hs)));
     title(["convergence plot for " scheme_name]);
     legend("L2error vs h", [num2str(exp(p(2))) " * h^" num2str(p(1))], "location", "northwest");
     xlabel("log(h)");
